8909c179fbebcc752b49e964c105fb6c6b69e203
lauoisin <lmendez2910@gmail.com>
2025-05-27 20:49:03 -0300

    Agrego lista y busqueda

---
+++ busqueda.py
@@ -0,0 +1,14 @@
+# Listas 
+
+supermercado = ["fideos", "arroz", "harina", "cafe", "yerba"]
+
+# Busqueda  
+
+producto_a_buscar = "cafe"
+if producto_a_buscar in supermercado:
+    print(f"{producto_a_buscar} está en la lista")
+else:
+    print(f"{producto_a_buscar} no está en la lista")
+
+# Busqueda 2
+
